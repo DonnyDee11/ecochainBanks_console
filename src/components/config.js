d9f6cd2fab14a14a5ade2ae91d6f2a8c116180b9
Donel Chihoma <dchihoma8@gmail.com>
2024-09-10 11:52:10 +0200

    Second Prep

--- src/components/config.js
+++ src/components/config.js
@@ -3,7 +3,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-  backendApiUrl: isProduction ? "https://ecochainbanks-api.onrender.com/" : "http://127.0.0.1:5000",
+  backendApiUrl: isProduction ? "https://ecochainbanks-api.onrender.com" : "http://127.0.0.1:5000",
 };
 
 export default config;
